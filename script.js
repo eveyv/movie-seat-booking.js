004075e1adda498a278fc133ecbc458ae0f64321
Everett Yeaw <evyeaw@gmail.com>
2020-04-14 22:38:07 -0400

    event listeners for seat and movie select

---
+++ script.js
@@ -0,0 +1,34 @@
+const container = document.querySelector('.container');
+const seats = document.querySelectorAll('.row.seat:not(.occupied)');
+const count = document.getElementById('count');
+const total = document.getElementById('total');
+const movieSelect = document.getElementById('movie');
+
+let ticketPrice = +movieSelect.value;
+//could also use parseInt() above
+
+updateSelectedCount = () => {
+  const selectedSeats = document.querySelectorAll('.row .seat.selected')
+
+  const selectedSeatsCount = selectedSeats.length;
+
+  count.innerText = selectedSeatsCount;
+  total.innerText = selectedSeatsCount * ticketPrice
+}
+
+movieSelect.addEventListener('change', event => {
+  ticketPrice = +event.target.value;
+  updateSelectedCount();
+});
+
+
+container.addEventListener('click', event => {
+  if(
+    event.target.classList.contains('seat') && !event.target.classList.contains('occupied')
+  ) {
+    event.target.classList.toggle('selected');
+
+
+    updateSelectedCount();
+  }
+});
