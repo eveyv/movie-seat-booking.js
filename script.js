8ba3ce16367ecf53ce40ee7bcbda6ed082650829
Everett Yeaw <evyeaw@gmail.com>
2020-04-18 21:28:36 -0400

    loop through selected seats and take movie index from local storage

--- script.js
+++ script.js
@@ -31,6 +31,18 @@ updateSelectedCount = () => {
 // pull data from local localStorage
 populateUI = () => {
   const selectedSeats = JSON.parse(localStorage.getItem('selectedSeats'));
+  if(selectedSeats !== null && selectedSeats.length > 0) {
+    seats.forEach((seat, index) => {
+      if(selectedSeats.indexOf(index) > -1) {
+        seat.classList.add('selected')
+      }
+    })
+  }
+
+  const selectedMovieIndex = localStorage.getItem('selectedMovieIndex');
+  if(selectedMovieIndex !== null) {
+    movieSelect.selectedIndex = selectedMovieIndex
+  }
 };
 
 movieSelect.addEventListener('change', event => {
@@ -50,3 +62,5 @@ container.addEventListener('click', event => {
     updateSelectedCount();
   }
 });
+//initial count and total set
+updateSelectedCount()
